2ae6d87c5edc02566d9dbb5d8e49689646ca7998
songboisgood <liaosongbo@qq.com>
2017-11-21 21:17:33 +0800

    scala init code

--- src/main/scala/Master.scala
+++ src/main/scala/Master.scala
@@ -1,10 +1,11 @@
 import akka.actor.Actor
-import scala.collection.mutable.Queue
+
+import scala.collection.mutable
 
 case class Worker(id: String)
 
 class Master extends Actor {
-  val workers: Queue[String] = Queue()
+  val workers: mutable.Queue[String] = mutable.Queue()
 
   def receive = {
     case Worker(id) => workers.enqueue(id)
